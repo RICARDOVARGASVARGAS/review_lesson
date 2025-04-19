853530fac9cc3ca008fa789e84e04bdc10f7eaa1
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2025-04-19 12:23:35 -0500

    Learn

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import { yarg } from "./config/plugins/yars.plugin";
-
-// console.log(process.argv);
-// console.log("!!!!!!!!!!!!!");
-// console.log(yarg.b);
+import { ServerApp } from "./presentation/server-app";
 
 (async () => {
   await main();
 })();
 
 async function main() {
-  console.log(yarg);
+  const { b: base, l: limit, s: showTable } = yarg;
+
+  ServerApp.run({
+    base,
+    limit,
+    showTable,
+  });
 }
